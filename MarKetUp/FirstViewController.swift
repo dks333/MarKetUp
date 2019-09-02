b9b0cc685a570e772335e38e2cdce3343e7deefa
dks333 <kaishan333sam@gmail.com>
2019-09-02 11:52:20 -0700

    Upload Project

---
+++ MarKetUp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MarKetUp
+//
+//  Created by Sam Ding on 9/2/19.
+//  Copyright © 2019 Kaishan Ding. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
