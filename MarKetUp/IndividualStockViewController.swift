6aca0d95f28a7d18723ba4212b9432871d8af7d8
dks333 <kaishan333sam@gmail.com>
2019-09-18 19:24:59 -0700

    Added a few features 2

---
+++ MarKetUp/IndividualStockViewController.swift
@@ -0,0 +1,54 @@
+//
+//  IndividualStockViewController.swift
+//  MarKetUp
+//
+//  Created by Sam Ding on 9/16/19.
+//  Copyright © 2019 Kaishan Ding. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class IndividualStockViewController: UIViewController {
+
+    @IBOutlet weak var stockNameLbl: UILabel!
+    @IBOutlet weak var priceLbl: UILabel!
+    @IBOutlet weak var percentageLbl: UILabel!
+    
+    var stockName = ""
+    var stockPrice = Float(0.0)
+    var stockPercentage = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+        self.setTabBarVisible(visible: false, animated: true)
+        super.viewWillAppear(animated)
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+        self.setTabBarVisible(visible: true, animated: true)
+        super.viewWillDisappear(animated)
+    }
+    
+    private func setupView(){
+        // Make navigation bar transparent
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        navigationController?.navigationBar.topItem?.title = ""
+        
+        stockNameLbl.text = stockName
+        priceLbl.text = String("\(stockPrice)")
+        percentageLbl.text = stockPercentage
+        
+    }
+
+    
+
+
+}
---
+++ MarKetUp/Supporting Files/UIKitExtensions.swift
@@ -0,0 +1,40 @@
+//
+//  UIKitExtensions.swift
+//  MarKetUp
+//
+//  Created by Sam Ding on 9/16/19.
+//  Copyright © 2019 Kaishan Ding. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    func setTabBarVisible(visible: Bool, animated: Bool) {
+        //* This cannot be called before viewDidLayoutSubviews(), because the frame is not set before this time
+        // bail if the current state matches the desired state
+        if (isTabBarVisible == visible) { return }
+        
+        // get a frame calculation ready
+        let frame = self.tabBarController?.tabBar.frame
+        let height = frame?.size.height
+        let offsetY = (visible ? -height! : height)
+        
+        // zero duration means no animation
+        let duration: TimeInterval = (animated ? 0.3 : 0.0)
+        
+        //  animate the tabBar
+        if frame != nil {
+            UIView.animate(withDuration: duration) {
+                self.tabBarController?.tabBar.frame = frame!.offsetBy(dx: 0, dy: offsetY!)
+                return
+            }
+        }
+    }
+    
+    var isTabBarVisible: Bool {
+        return (self.tabBarController?.tabBar.frame.origin.y ?? 0) < self.view.frame.maxY
+    }
+    
+}
